a05b6b941c2223f5d2459af42bd7701c05acf095
psicologanicollyoliveira-del <psicologa.nicollyoliveira@gmail.com>
2025-11-02 03:35:43 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        {/* Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17697138635"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17697138635');
+          `}
+        </Script>
+      </head>
       <body
         className={`${quicksand.variable} ${fraunces.variable} antialiased`}
       >
